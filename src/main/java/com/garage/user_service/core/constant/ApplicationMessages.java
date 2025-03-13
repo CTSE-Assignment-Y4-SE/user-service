1c0008429c68fecf2be73ef8a280508b90e5f6c5
harith2001 <danula243@gmail.com>
2025-03-13 14:26:12 +0530

    Application variables added

---
+++ src/main/java/com/garage/user_service/core/constant/ApplicationMessages.java
@@ -0,0 +1,26 @@
+package com.garage.user_service.core.constant;
+
+public final class ApplicationMessages {
+
+	private ApplicationMessages() {
+	}
+
+	public static final String ERROR_GARAGE_ADMIN_ALREADY_EXISTS = "api.error.garage-admin.already-exists";
+
+	public static final String ERROR_USER_NOT_FOUND = "api.error.user.not.found";
+
+	public static final String ERROR_USER_ALREADY_EXISTS = "api.error.user.already-exists";
+
+	public static final String ERROR_VEHICLE_OWNER_CANNOT_BE_CREATED = "api.error.vehicle-owner.cannot-be-created";
+
+	public static final String ERROR_INVALID_CURRENT_PASSWORD = "api.error.invalid.current-password";
+
+	public static final String ERROR_USER_ACCOUNT_DEACTIVATED = "api.error.user.account-deactivated";
+
+	public static final String ERROR_PERMISSION_DENIED = "api.error.permission-denied";
+
+	public static final String ERROR_OTP_EXPIRED = "api.error.otp.expired";
+
+	public static final String ERROR_INVALID_OTP = "api.error.invalid.otp";
+
+}
